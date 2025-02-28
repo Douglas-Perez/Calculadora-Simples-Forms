b13dd6a62fedbe02328479e443f4beb4f608a53d
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-02-28 20:51:09 -0300

    Criar estrutura base

--- Calculadora Simples Forms/Form1.Designer.cs
+++ Calculadora Simples Forms/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            campo1 = new TextBox();
+            campo2 = new TextBox();
+            soma = new Button();
+            subtracao = new Button();
+            multiplicacao = new Button();
+            divisao = new Button();
+            operado = new Label();
+            resultado = new Label();
+            SuspendLayout();
+            // 
+            // campo1
+            // 
+            campo1.Location = new Point(12, 22);
+            campo1.Name = "campo1";
+            campo1.Size = new Size(92, 23);
+            campo1.TabIndex = 0;
+            // 
+            // campo2
+            // 
+            campo2.Location = new Point(142, 22);
+            campo2.Name = "campo2";
+            campo2.Size = new Size(90, 23);
+            campo2.TabIndex = 1;
+            // 
+            // soma
+            // 
+            soma.Location = new Point(12, 82);
+            soma.Name = "soma";
+            soma.Size = new Size(101, 23);
+            soma.TabIndex = 2;
+            soma.Text = "Soma";
+            soma.UseVisualStyleBackColor = true;
+            // 
+            // subtracao
+            // 
+            subtracao.Location = new Point(131, 82);
+            subtracao.Name = "subtracao";
+            subtracao.Size = new Size(101, 23);
+            subtracao.TabIndex = 3;
+            subtracao.Text = "Subtração";
+            subtracao.UseVisualStyleBackColor = true;
+            // 
+            // multiplicacao
+            // 
+            multiplicacao.Location = new Point(12, 111);
+            multiplicacao.Name = "multiplicacao";
+            multiplicacao.Size = new Size(101, 23);
+            multiplicacao.TabIndex = 4;
+            multiplicacao.Text = "Multiplicação";
+            multiplicacao.UseVisualStyleBackColor = true;
+            // 
+            // divisao
+            // 
+            divisao.Location = new Point(131, 111);
+            divisao.Name = "divisao";
+            divisao.Size = new Size(101, 23);
+            divisao.TabIndex = 5;
+            divisao.Text = "Divisão";
+            divisao.UseVisualStyleBackColor = true;
+            // 
+            // operado
+            // 
+            operado.Location = new Point(110, 21);
+            operado.Name = "operado";
+            operado.Size = new Size(26, 23);
+            operado.TabIndex = 6;
+            operado.Text = "?";
+            operado.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // resultado
+            // 
+            resultado.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            resultado.Location = new Point(12, 48);
+            resultado.Name = "resultado";
+            resultado.Size = new Size(220, 31);
+            resultado.TabIndex = 7;
+            resultado.Text = "Resultado";
+            resultado.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(244, 142);
+            Controls.Add(resultado);
+            Controls.Add(operado);
+            Controls.Add(divisao);
+            Controls.Add(multiplicacao);
+            Controls.Add(subtracao);
+            Controls.Add(soma);
+            Controls.Add(campo2);
+            Controls.Add(campo1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox campo1;
+        private TextBox campo2;
+        private Button soma;
+        private Button subtracao;
+        private Button multiplicacao;
+        private Button divisao;
+        private Label operado;
+        private Label resultado;
     }
 }
